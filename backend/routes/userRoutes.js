fc5f5316708f4fe090c96cbc856baf7bef31fa1f
enterpannet <enterpannet@gmail.com>
2024-11-09 22:49:35 +0700

    edit router

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,6 +5,6 @@ const router = express.Router();
 
 router.get('/users', getUsers);
 router.post('/users', createUser);
-router.delete('/users:id', deleteUser);
+router.delete('/users/:id', deleteUser);
 
 export default router;
