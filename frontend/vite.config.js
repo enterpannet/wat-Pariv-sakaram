2f33b730583a7e3a930cac6dc95414ee8bd9d8f1
enterpannet <enterpannet@gmail.com>
2024-11-09 22:28:11 +0700

    edit qrcode

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // Allows external access
-    port: 3000,      // Optional: Set a custom port if needed
-  },
-})
+    host: '0.0.0.0', // ให้ Vite ฟังทุกการเชื่อมต่อจากภายนอก
+    port: 3000,
+    hmr: {
+      host: '210.246.215.231', // ใช้ IP หรือโดเมนของ VPS
+      port: 3000
+    }
+  }
+});
