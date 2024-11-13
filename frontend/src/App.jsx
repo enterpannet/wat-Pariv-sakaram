0048311fcc5134bda4d0ccfe9ef014f5e43c914a
enterpannet <enterpannet@gmail.com>
2024-11-13 23:58:03 +0700

    edit some comment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100">
-      <h1 className="text-3xl font-bold text-gray-800 mb-6">Navigation Menu</h1>
+    <div className="flex flex-col justify-center items-center mt-5">
+     
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
       <Link to="/" className="nav-button">
           หน้าแรก
