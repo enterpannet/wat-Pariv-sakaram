87f89376a2e30203307d0d9bf034c1a09d306402
enterpannet <enterpannet@gmail.com>
2024-11-13 18:57:22 +0700

    edit some comment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,30 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'วัดหนองขนาก',
+        short_name: 'WNKN',
+        description: 'www.wnkn.org',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'wnkn192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'wnkn512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
 });
